71be0d6856747f29ea8a4649eb7e583c84a0de5b
roomacarthur <roomacarthur@gmail.com>
2022-03-05 21:46:48 +0000

    set up enviromentals in settings.py and hooked up postgresSQL from heroku

--- pcr/settings.py
+++ pcr/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 if os.path.isfile('env.py'):
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pcr-roo.herokuapp.com']
+ALLOWED_HOSTS = ['pcr-roo.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -79,10 +80,7 @@ WSGI_APPLICATION = 'pcr.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
