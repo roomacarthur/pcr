c0331257f8fb7f1c17c674d7bc0d47d281993a7a
roomacarthur <roomacarthur@gmail.com>
2022-03-15 20:58:45 +0000

    added basic views for necassery pages

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>Homepage.</h1>")
+
+def all_posts(request):
+    return HttpResponse("<h1>All Posts</h1>")
+
+def post_details(request):
+    return HttpResponse("<h1>Post Details</h1>")
+
+def user_profile(request):
+    return HttpResponse("<h1>Profile</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About</h1>")
+
+def help(request):
+    return HttpResponse("<h1>Help Page</h1>")
