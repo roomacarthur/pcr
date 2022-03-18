0e719fedd04e2695e5e4f6bdf157b23bd2390506
roomacarthur <roomacarthur@gmail.com>
2022-03-18 19:42:20 +0000

    removed commented out code from settings.py

--- pcr/settings.py
+++ pcr/settings.py
@@ -164,13 +164,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'live-static-files', 'static-root')
-
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static-files', 'media-root' )
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
