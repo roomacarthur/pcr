996b4feadaa64a1655c7f11f14e9d286ef782ea5
roomacarthur <roomacarthur@gmail.com>
2022-03-21 22:03:59 +0000

    updated admin panel for better functionality

--- posts/admin.py
+++ posts/admin.py
@@ -5,8 +5,15 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content')
+    prepopulated_fields = {"slug": ('title',)}
+    summernote_fields = ("content")
+    list_filter = ("updated_on", "created_on")
+    list_display = ("title", "author", "created_on", "updated_on")
+    search_fields = ["author", "title", "content"]
 
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
-    summernote_fields = ('review')
+    summernote_fields = ("review")
+    list_filter = ("updated_on", "created_on")
+    list_display = ("name", "created_on")
+    search_fields = ["name", "review"]
