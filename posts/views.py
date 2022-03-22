3c13897850f436267ef4923d2cda75861e0180ea
roomacarthur <roomacarthur@gmail.com>
2022-03-22 22:18:09 +0000

    added posts list view for all posts

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from django.views import generic
+from urllib3 import add_stderr_logger
+from .models import Post
 
 def home(request):
     return render(request, 'posts/index.html')
 
-def all_posts(request):
-    return render(request, 'posts/all_posts.html')
+# def all_posts(request):
+#     return render(request, 'posts/all_posts.html')
 
 def post_details(request):
     return render(request, 'posts/post_details.html')
@@ -20,3 +21,10 @@ def help(request):
 
 def new_post(request):
     return render(request, 'posts/new_post.html')
+
+# all posts view.
+class Posts(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(approved=True).order_by('-created_on')
+    template_name = 'posts/all_posts.html'
+    paginate_by = 10
