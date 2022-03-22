5087bafccb23b5c4dbae09b91a6ff52635cfedf9
roomacarthur <roomacarthur@gmail.com>
2022-03-22 23:59:20 +0000

    added all posts and post details urlpatterns

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="homepage"),
-    path('all/', views.Posts.as_view(), name="all-posts"),
-    path('posts/', views.post_details, name="post-details"),
+    path('all/', views.AllPosts.as_view(), name="all-posts"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name="post-details"),
     path('about/', views.about, name="about-page"),
     path('help/', views.help, name="help-page"),
     path('new_post/', views.new_post, name="new-post")
