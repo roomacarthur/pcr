de4b1f60a06a811c4b85e6847c899d7aea3335f6
roomacarthur <roomacarthur@gmail.com>
2022-03-22 20:56:59 +0000

    updated admin.py classes, admin panel functionality and post/review approval for moeration

--- posts/admin.py
+++ posts/admin.py
@@ -4,15 +4,26 @@ from .models import Post, Review
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ("updated_on", "created_on")
-    list_display = ("title", "author", "created_on", "updated_on")
+    list_filter = ("updated_on", "created_on", "approved")
+    list_display = ("title", "author", "created_on", "updated_on", "approved")
     search_fields = ["author", "title", "content"]
+    actions = ["approve_posts", "unapprove_posts"]
+
+    def approve_posts(self, request, queryset):
+        queryset.update(approved=True)
+    
+    def unapprove_posts(self, request, queryset):
+        queryset.update(approved=False)
 
 admin.site.register(Post, PostAdmin)
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_filter = ("updated_on", "created_on")
-    list_display = ("name", "created_on")
+    list_filter = ("updated_on", "created_on", "approved")
+    list_display = ("name", "created_on", "approved")
     search_fields = ["name", "review"]
+    actions = ["approve_comments"]
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=False)
 
 admin.site.register(Review, ReviewAdmin)
