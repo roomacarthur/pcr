cbbb0c581a629bc62c934314a48d4bf02c3db0ac
roomacarthur <roomacarthur@gmail.com>
2022-03-22 23:58:53 +0000

    added postdetail cbv

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import generic
-from urllib3 import add_stderr_logger
+from django.views import generic, View
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 def home(request):
     return render(request, 'posts/index.html')
-
-# def all_posts(request):
-#     return render(request, 'posts/all_posts.html')
 
 def post_details(request):
     return render(request, 'posts/post_details.html')
@@ -22,9 +19,33 @@ def help(request):
 def new_post(request):
     return render(request, 'posts/new_post.html')
 
-# all posts view.
-class Posts(generic.ListView):
+# all posts view, used within all_posts.html
+class AllPosts(generic.ListView):
     model = Post
     queryset = Post.objects.filter(approved=True).order_by('-created_on')
     template_name = 'posts/all_posts.html'
     paginate_by = 10
+# posts view for homepage
+
+# post detail view.
+# with help from  "therefor I blog"
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(approved=True)
+        post = get_object_or_404(queryset, slug=slug)
+        reviews = post.reviews.filter(approved=True).order_by('-created_on')
+        votes = False
+        
+        if post.likes.filter(id=self.request.user.id).exists():
+            votes=True
+        
+        return render(
+            request, 'posts/post_details.html',
+            {
+                "post": post,
+                "reviews": reviews,
+                "votes": votes
+            },
+        )
+        
