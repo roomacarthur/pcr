1918557a98f5cf00dfc4ffdf0b156fbd9c4f5ac2
roomacarthur <roomacarthur@gmail.com>
2022-03-22 22:18:29 +0000

    added urlpatter for posts list view.

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="homepage"),
-    path('all/', views.all_posts, name="all-posts"),
+    path('all/', views.Posts.as_view(), name="all-posts"),
     path('posts/', views.post_details, name="post-details"),
     path('about/', views.about, name="about-page"),
     path('help/', views.help, name="help-page"),
