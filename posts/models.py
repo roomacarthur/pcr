9567b0da6fe76eafdcd119c997e087bca1b5321d
roomacarthur <roomacarthur@gmail.com>
2022-03-23 10:45:00 +0000

    set post to null in review model

--- posts/models.py
+++ posts/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
         return self.likes.count()
 
 class Review(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
     name = models.ForeignKey(User, on_delete=models.CASCADE,related_name="review")
     review = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
