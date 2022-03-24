1867dda583abe48296d1d95d51956cd70a80e027
roomacarthur <roomacarthur@gmail.com>
2022-03-24 13:54:10 +0000

    ability for user to create new post

---
+++ posts/forms.py
@@ -0,0 +1,12 @@
+from .models import Post, Review
+from django import forms
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("author", "title", "content", "live_link", "repo_link",)
+        
+class ReviewForm(forms.ModelForm):
+    class Meta: 
+        model = Review
+        fields = ("body",)
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic, View
+from django.utils.text import slugify
 from django.shortcuts import get_object_or_404, render
 from .models import Post, Review
+from .forms import PostForm, ReviewForm
+
 
 def home(request):
     return render(request, 'posts/index.html')
@@ -12,9 +15,7 @@ def about(request):
 
 def help(request):
     return render(request, 'posts/help.html')
-
-def new_post(request):
-    return render(request, 'posts/new_post.html')
+# homepage view.
 
 # all posts view, used within all_posts.html
 class AllPosts(generic.ListView):
@@ -31,7 +32,7 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(approved=True)
         post = get_object_or_404(queryset, slug=slug)
-        reviews = Review.objects.filter(approved=True).order_by('-created_on')
+        reviews = Review.objects.filter(approved=True).order_by('created_on')
         likes = False
         
         if post.likes.filter(id=self.request.user.id).exists():
@@ -42,7 +43,51 @@ class PostDetail(View):
             {
                 "post": post,
                 "reviews": reviews,
-                "likes": likes
+                "likes": likes,
+                "review_form": ReviewForm()
             },
         )
         
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(approved=True)
+        post = get_object_or_404(queryset, slug=slug)
+        reviews = Review.objects.filter(approved=True).order_by('created_on')
+        likes = False
+        
+        if post.likes.filter(id=self.request.user.id).exists():
+            likes=True
+
+        review_form = ReviewForm(data=request.POST)
+
+        if review_form.is_valid():
+            review_form.instance.name = request.user.username
+            review = review_form.save(commit=False)
+            review.post = post
+            review.save()
+        else:
+            review_form = ReviewForm()
+        
+        return render(
+            request, 'posts/post_details.html',
+            {
+                "post": post,
+                "reviews": reviews,
+                "likes": likes,
+                "review_form": ReviewForm()
+            },
+        )
+
+# allow user to submit a new post to the site.
+def new_post(request):
+    user = request.user.id
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.author = user
+            form.save()
+            return HttpResponseRedirect("/")
+    else:
+        form = PostForm()
+
+    return render(request, "posts/new_post.html", {"form": form})
+
