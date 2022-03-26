d08d18905032de39afa3410f0ef60bdc821e682f
roomacarthur <roomacarthur@gmail.com>
2022-03-26 10:01:21 +0000

    cleaned up forms.py to be pep8 compliant

--- posts/forms.py
+++ posts/forms.py
@@ -1,13 +1,15 @@
 from .models import Post, Review
 from django import forms
 
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ("title", "content", "live_link", "repo_link",)
-        
+
+
 class ReviewForm(forms.ModelForm):
-    class Meta: 
+    class Meta:
         model = Review
         fields = ("body",)
         labels = {
