7a340c5a7bbc4744ed1a2297e5f274e134f097fb
roomacarthur <roomacarthur@gmail.com>
2022-03-26 10:00:38 +0000

    cleaned up models.py to be pep8 compliant

--- posts/models.py
+++ posts/models.py
@@ -3,11 +3,16 @@ from django.contrib.auth.models import User
 from django.utils.text import slugify
 from ckeditor.fields import RichTextField
 
+
 # Main Post model.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=150, unique=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    slug = models.SlugField(
+        max_length=150, unique=True, blank=True
+        )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="post"
+        )
     content = RichTextField(blank=True, null=True)
     live_link = models.URLField(max_length=200, blank=True)
     repo_link = models.URLField(max_length=200, blank=True)
@@ -27,28 +32,32 @@ class Post(models.Model):
     # Helper to return the total count of likes on post.
     def total_likes(self):
         return self.likes.count()
-    
+
     # Helper function to auto save the slug from title when submitting form.
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-        return super().save(*args,**kwargs)
+        return super().save(*args, **kwargs)
+
 
 # Review Model.
 class Review(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="reviews")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="reviews"
+        )
     name = models.CharField(max_length=50)
     body = RichTextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    votes = models.ManyToManyField(User, related_name="Review_likes", blank=True)
+    votes = models.ManyToManyField(
+        User, related_name="Review_likes", blank=True
+        )
     approved = models.BooleanField(default=True)
 
-    # Order posts by creation date in ascending order. 
+    # Order posts by creation date in ascending order.
     class Meta:
         ordering = ["created_on"]
 
     # Helper to return an F string representation of object.
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
-    
