f13e8d4edd5f25ea3630ab050d6f0ce9746b08d3
roomacarthur <roomacarthur@gmail.com>
2022-03-26 10:02:01 +0000

    jQuery for styling django forms when rendered in view

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 function toggleMobileNav(menu) {
   menu.classList.toggle("open");
 }
+
+// New Post Form, place holder over rides. 
+$("input#id_title").attr("placeholder", "Enter a title...")
+$("input#id_live_link").attr("placeholder", "Enter a Live link...(https://...)")
+$("input#id_repo_link").attr("placeholder", "Enter a Repo link...(https://...)")
+
+// Signup form, place holder over rides 
+$("input#id_repo_link").attr("placeholder", "Enter a Repo link...(https://...)")
