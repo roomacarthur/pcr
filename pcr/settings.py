b658a6577236efbd039d4c8a7b09a862e4986180
roomacarthur <roomacarthur@gmail.com>
2022-03-26 11:49:54 +0000

    whitenoise fix

--- pcr/settings.py
+++ pcr/settings.py
@@ -163,7 +163,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Custom configs for CKeditor, control the style and look of the Rich Text Editor. 
 # https://ckeditor.com/docs/ckeditor4/latest/index.html
